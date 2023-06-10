830e4903436a0c04f20db0c872c07a1e0dc49f8e
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-06-10 18:58:40 +0600

    error page 404 picture shown

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+import img from "../../assets/img404.jpg";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <img src={img} className="w-full max-h-screen" alt="" />
+    </div>
+  );
+}
